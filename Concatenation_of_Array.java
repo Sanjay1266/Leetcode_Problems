fd0fc90cac5bde156e18760808192c6f1ffb8b99
Sanjay1266 <sanjaysundaram50@gmail.com>
2026-04-15 00:31:56 +0530

    Completed the problem add two numbers

---
+++ Concatenation_of_Array.java
@@ -0,0 +1,11 @@
+public class Concatenation_of_Array {
+    public static void main(String[] args) {
+        int [] num = {1,2,3};
+        int i;
+        len = num.length;
+        for (i=0;i<len;i++){
+
+        }
+
+    }
+}
